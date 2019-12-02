084022860552e86645e675b113e1b83e72757ff4
Hi Tran <thuchitran@gmail.com>
2019-12-03 01:52:30 +0700

    styling, add progress bar to music player

--- src/MusicPlayer/MusicPlayer.jsx
+++ src/MusicPlayer/MusicPlayer.jsx
@@ -1,37 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './MusicPlayer.module.scss';
 import { faPlay, faForward, faBackward, faPause } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import findingHope from './assets/finding_hope_3AM.mp3';
 
 function MusicPlayer() {
     const [isPlaying, setPlayingState] = useState(false);
+    const [currentTime, setCurrentTime] = useState('0:00');
+    const [duration, setDuration] = useState('0:00');
+    let musicAudio = React.createRef();
+    let currentProgress = React.createRef();
+
     const handlePlayBtnClick = () => {
         setPlayingState(!isPlaying);
+        isPlaying ? musicAudio.current.pause() : musicAudio.current.play();
     }
+    const updateTime = () => {
+        const time = Math.floor(musicAudio.current.currentTime);
+        setCurrentTime(formatTime(time));
+        // update progress bar
+        const current = musicAudio.current.currentTime;
+        const total = musicAudio.current.duration;
+        currentProgress.current.style.width = `${(current / total) * 170}px`;
+        if(current === total) {
+            setPlayingState(false);
+        }
+    }
+
+    const formatTime = (time) => {
+        const minute = `${Math.floor(time / 60)}`;
+        time = time % 60;
+        let second;
+        if (time < 10) {
+            second = `0${time}`
+        } else {
+            second = `${time}`;
+        }
+        return (`${minute}:${second}`)
+    }
+
+    useEffect(() => {
+        if (musicAudio.current && musicAudio.current.duration) {
+            let musicDuration = Math.floor(musicAudio.current.duration);
+            setDuration(formatTime(musicDuration));
+        }
+    }, [musicAudio]);
+
     return (
-        <div>
+        <div className={styles.MusicPlayer}>
             <h1 className="Title">Music Player</h1>
             <p>This simple music player allows user to play/pause, go to the next/previous song</p>
-            <div className={styles.MusicPlayer}>
-                <div className={styles.SongInfo}>
-                    <p><b>Now Playing</b></p>
-                    <p>Finding Hope by 3AM</p>
-                    <audio controls>
-                        <source src="horse.mp3" type="audio/mpeg"/>
-                    </audio>
+            <audio ref={musicAudio} onTimeUpdate={updateTime}>
+                <source src={findingHope} type="audio/mpeg" />
+            </audio>
+            <div className={styles.SongInfo}>
+                <p><b>Finding Hope</b></p>
+                <p>3 AM</p>
+                <div className={styles.SongDuration}>
+                    <p>{currentTime}</p>
+                    <p>{duration}</p>
                 </div>
-                        <div className={styles.MusicPlayerControls}>
-
-                            <span><FontAwesomeIcon icon={faBackward} /></span>
-                            <div className={`${styles.PlayBtnWrapper} ${isPlaying ? styles.Playing : ''}`} onClick={handlePlayBtnClick}>
-                                <div className={styles.RotateCircle}></div>
-                                <span className={styles.PlayBtn}><FontAwesomeIcon icon={isPlaying ? faPause : faPlay} /></span>
-                            </div>
-                            <span><FontAwesomeIcon icon={faForward} /></span>
-                        </div>
+                <div className={styles.ProgressBar}>
+                    <div ref={currentProgress} className={styles.CurrentProgress}></div>
+                </div>
             </div>
+            <div className={styles.MusicPlayerControls}>
+                <div className={styles.RotateCircle}>
+                    <span></span>
                 </div>
-                )
-            }
-            
+                <span><FontAwesomeIcon icon={faBackward} /></span>
+                <span className={styles.PlayBtn}><FontAwesomeIcon onClick={handlePlayBtnClick} icon={isPlaying ? faPause : faPlay} /></span>
+                <span><FontAwesomeIcon icon={faForward} /></span>
+            </div>
+        </div>
+    )
+}
+
 export default MusicPlayer;

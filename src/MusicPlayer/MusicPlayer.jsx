21be6dc198318bc199cd9e308da12911bca29b30
Hi Tran <thuchitran@gmail.com>
2019-12-03 21:35:09 +0700

    handle volume on/off

--- src/MusicPlayer/MusicPlayer.jsx
+++ src/MusicPlayer/MusicPlayer.jsx
@@ -1,13 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './MusicPlayer.module.scss';
-import { faPlay, faForward, faBackward, faPause } from "@fortawesome/free-solid-svg-icons";
+import { faPlay, faForward, faBackward, faPause, faVolumeMute, faVolumeUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import findingHope from './assets/finding_hope_3AM.mp3';
+import findingHopeAudio from './assets/finding_hope_3AM.mp3';
+import findingHopeImage from './assets/finding_hope.jpg';
+import robinHoodAudio from './assets/RobinHood.mp3';
+
 
 function MusicPlayer() {
+    const songs = [
+        {
+            id: 1,
+            name: 'Finding Hope',
+            artist: '3 AM',
+            duration: '3:22',
+            src: findingHopeAudio
+        },
+        {
+            id: 2,
+            name: 'Robin Hood',
+            artist: 'Anson Seabra',
+            duration: '3:42',
+            src: robinHoodAudio
+        }
+    ];
     const [isPlaying, setPlayingState] = useState(false);
     const [currentTime, setCurrentTime] = useState('0:00');
-    const [duration, setDuration] = useState('0:00');
+    const [currentSong, setCurrentSong] = useState(songs[0]);
+    const [isMuted, setMutedState] = useState(false);
     let musicAudio = React.createRef();
     let currentProgress = React.createRef();
 
@@ -15,14 +35,15 @@ function MusicPlayer() {
         setPlayingState(!isPlaying);
         isPlaying ? musicAudio.current.pause() : musicAudio.current.play();
     }
-    const updateTime = () => {
-        const time = Math.floor(musicAudio.current.currentTime);
+
+    const updateTime = (time = musicAudio.current.currentTime) => {
+        time = Math.floor(musicAudio.current.currentTime);
         setCurrentTime(formatTime(time));
         // update progress bar
         const current = musicAudio.current.currentTime;
         const total = musicAudio.current.duration;
         currentProgress.current.style.width = `${(current / total) * 170}px`;
-        if(current === total) {
+        if (current === total) {
             setPlayingState(false);
         }
     }
@@ -39,26 +60,40 @@ function MusicPlayer() {
         return (`${minute}:${second}`)
     }
 
-    useEffect(() => {
-        if (musicAudio.current && musicAudio.current.duration) {
-            let musicDuration = Math.floor(musicAudio.current.duration);
-            setDuration(formatTime(musicDuration));
+    const switchSong = () => {
+        switch (currentSong.id) {
+            case 1:
+                setCurrentSong(songs[1]);
+                break;
+            case 2:
+                setCurrentSong(songs[0]);
+                break;
+            default:
+                setCurrentSong(songs[0]);
         }
-    }, [musicAudio]);
+        currentProgress.current.style.width = 0;
+        musicAudio.current.load();
+        setPlayingState(false);
+    }
+
+    const changeVolume = () => {
+        setMutedState(!isMuted);
+        musicAudio.current.muted = !isMuted;
+    }
 
     return (
         <div className={styles.MusicPlayer}>
             <h1 className="Title">Music Player</h1>
-            <p>This simple music player allows user to play/pause, go to the next/previous song</p>
+            <p>A simple music player</p>
             <audio ref={musicAudio} onTimeUpdate={updateTime}>
-                <source src={findingHope} type="audio/mpeg" />
+                <source src={currentSong.src} type="audio/mpeg" />
             </audio>
             <div className={styles.SongInfo}>
-                <p><b>Finding Hope</b></p>
-                <p>3 AM</p>
+                <p><b>{currentSong.name}</b></p>
+                <p>{currentSong.artist}</p>
                 <div className={styles.SongDuration}>
                     <p>{currentTime}</p>
-                    <p>{duration}</p>
+                    <p>{currentSong.duration}</p>
                 </div>
                 <div className={styles.ProgressBar}>
                     <div ref={currentProgress} className={styles.CurrentProgress}></div>
@@ -66,11 +101,21 @@ function MusicPlayer() {
             </div>
             <div className={styles.MusicPlayerControls}>
                 <div className={styles.RotateCircle}>
+                    <img src={findingHopeImage} alt="poster" />
                     <span></span>
                 </div>
-                <span><FontAwesomeIcon icon={faBackward} /></span>
-                <span className={styles.PlayBtn}><FontAwesomeIcon onClick={handlePlayBtnClick} icon={isPlaying ? faPause : faPlay} /></span>
-                <span><FontAwesomeIcon icon={faForward} /></span>
+                <span>
+                    <FontAwesomeIcon icon={faBackward} onClick={switchSong} />
+                </span>
+                <span className={styles.PlayBtn}>
+                    <FontAwesomeIcon onClick={handlePlayBtnClick} icon={isPlaying ? faPause : faPlay} />
+                </span>
+                <span>
+                    <FontAwesomeIcon icon={faForward} onClick={switchSong} />
+                </span>
+                <span>
+                    <FontAwesomeIcon icon={isMuted ? faVolumeMute : faVolumeUp} onClick={changeVolume} />
+                </span>
             </div>
         </div>
     )
